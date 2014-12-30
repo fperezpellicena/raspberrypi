5f28766ac8f09970a90c4b9fa792587332b2602e
fperezpellicena <fperezpellicena@gmail.com>
2014-12-30 18:43:37 +0100

    Check error conditions

--- producer-consumer/src/producer-consumer.cpp
+++ producer-consumer/src/producer-consumer.cpp
@@ -4,26 +4,30 @@
 // Version     :
 // Copyright   : 
 // Description : Producer consumer example using a queue
-// sudo mkdir /dev/mqueue
-// sudo mount -t mqueue none /dev/mqueue
 //============================================================================
-#include <iostream>
+
 #include <stdlib.h>
 #include <mqueue.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 
-#define QUEUE_NAME		"/producer-consumer"
-#define QUEUE_SIZE		2
-#define QUEUE_OFLAGS	O_RDWR | O_CREAT
-#define QUEUE_MODE		S_IRUSR | S_IWUSR
-#define QUEUE_PRIO		0
+#define QUEUE_DIR			"/tmp/producer-consumer"
+#define QUEUE_DIR_PERMS		(S_IRWXU | S_IRWXG | S_IRWXO)
 
-#define MESSAGE			"Hello!"
-#define MESSAGE_LENGTH	7
+#define QUEUE_NAME			"/producer-consumer"
+#define QUEUE_SIZE			2
+#define QUEUE_OFLAGS		(O_RDWR | O_CREAT)
+#define QUEUE_MODE			(S_IRUSR | S_IWUSR)
+#define QUEUE_PRIO			0
 
-#define PRODUCER_RATE	5
+#define MESSAGE				"Hello!"
+#define MESSAGE_LENGTH		7
+
+#define PRODUCER_RATE		2
 
 using namespace std;
 
@@ -33,9 +37,12 @@ static mqd_t queue;
 
 static void* produce(void*) {
 	while(true) {
+		if (mq_send(queue, MESSAGE, MESSAGE_LENGTH, QUEUE_PRIO) == -1) {
+			printf("Error sending message to the queue");
+			break;
+		}
+		printf("Producer thread sent: %s", MESSAGE);
 		sleep(PRODUCER_RATE);
-		mq_send(queue, MESSAGE, MESSAGE_LENGTH, QUEUE_PRIO);
-		cout << "Send" << endl;
 	}
 	pthread_exit(NULL);
 }
@@ -43,16 +50,21 @@ static void* produce(void*) {
 static void* consume(void*) {
 	char message[2 * MESSAGE_LENGTH];
 	while(true) {
-		mq_receive(queue, message, MESSAGE_LENGTH, NULL);
-		cout << "Receive: " << message << endl;
+		if (mq_receive(queue, message, MESSAGE_LENGTH, NULL) == -1) {
+			perror("Error receiving message from the queue");
+			break;
+		}
+		printf("Consumer thread received: %s", message);
 	}
 	pthread_exit(NULL);
 }
 
 static void signalHandler(int sig, siginfo_t *siginfo, void *context) {
-	cout << "Signal received: " << sig << endl;
+	// Close queue
 	mq_close(queue);
 	mq_unlink(QUEUE_NAME);
+	rmdir(QUEUE_DIR);
+	// Cancel threads
 	pthread_cancel(producer);
 	pthread_cancel(consumer);
 	abort();
@@ -63,12 +75,25 @@ static void createQueue(void) {
 	attr.mq_flags = 0;
 	attr.mq_maxmsg = QUEUE_SIZE;
 	attr.mq_msgsize = MESSAGE_LENGTH;
-	queue = mq_open(QUEUE_NAME, QUEUE_OFLAGS, QUEUE_MODE, &attr);
+	if ((queue = mq_open(QUEUE_NAME, QUEUE_OFLAGS, QUEUE_MODE, &attr)) == -1) {
+		perror("Error creating queue");
+		exit(EXIT_FAILURE);
+	}
+	if (mkdir(QUEUE_DIR, QUEUE_DIR_PERMS) == -1) {
+		perror("Error creating queue directory");
+		exit(EXIT_FAILURE);
+	}
 }
 
 static void createThreads(void) {
-	pthread_create(&producer, NULL, produce, NULL);
-	pthread_create(&consumer, NULL, consume, NULL);
+	if (pthread_create(&producer, NULL, produce, NULL) != 0) {
+		perror("Error creating producer thread");
+		exit(EXIT_FAILURE);
+	}
+	if (pthread_create(&consumer, NULL, consume, NULL) != 0) {
+		perror("Error creating consumer thread");
+		exit(EXIT_FAILURE);
+	}
 }
 
 static void configureSignalHandler(void) {
